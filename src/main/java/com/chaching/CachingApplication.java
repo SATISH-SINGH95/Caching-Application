e54e58e5e1373167fd3c464ac8a1ae00557daeca
Satish Kumar Singh <sk95satish@gmail.com>
2024-10-03 17:41:06 +0530

    Update CachingApplication.java

--- src/main/java/com/chaching/CachingApplication.java
+++ src/main/java/com/chaching/CachingApplication.java
@@ -1,6 +1,8 @@
 package com.chaching;
 
 import org.springframework.boot.SpringApplication;
+import org.slf4j.MDC;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,9 +10,11 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableCaching
 @EnableAspectJAutoProxy
+@Slf4j
 public class CachingApplication {
 
 	public static void main(String[] args) {
+		MDC.put("pid", String.valueOf(ProcessHandle.current().pid()));
 		SpringApplication.run(CachingApplication.class, args);
 	}
 
